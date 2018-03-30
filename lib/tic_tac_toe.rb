51f30ca6bfe9b53e7626666e3bf5b05f80289918
nancy sin <nsin@sbcglobal.net>
2018-03-30 01:43:07 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,24 +3,24 @@ class TicTacToe
     @board = Array.new(9, " ")
   end
 
-WIN_COMBINATIONS = [
- [0,1,2],
- [3,4,5],
- [6,7,8],
- [0,3,6],
- [1,4,7],
- [2,5,8],
- [0,4,8],
- [2,4,6]
-]
+  WIN_COMBINATIONS = [
+   [0,1,2],
+   [3,4,5],
+   [6,7,8],
+   [0,3,6],
+   [1,4,7],
+   [2,5,8],
+   [0,4,8],
+   [2,4,6]
+  ]
 
-def display_board
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-  puts "-----------"
-  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-  puts "-----------"
-  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-   end
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 
    def input_to_index(user_input)
      user_input.to_i - 1
@@ -56,11 +56,12 @@ def display_board
         turn_counter
       end
 
-        def current_player
-          turn_count.even? ? "X" : "O"
-        end
+      def current_player
+        turn_count.even? ? "X" : "O"
+      end
+
         def turn
-          puts "Pleae enter 1-9:"
+          puts "Please enter 1-9:"
           user_input = gets.strip
           index = input_to_index(user_input)
           if valid_move?(index)
@@ -70,37 +71,45 @@ def display_board
             turn
           end
         end
-          def won?
+
+        def won?
             WIN_COMBINATIONS.detect do |win_combination|
               @board[win_combination[0]] == @board[win_combination[1]] &&
               @board[win_combination[1]] == @board[win_combination[2]] &&
               position_taken?(win_combination[0])
             end
           end
+
           def full?
             @board.none? do |index|
               index == nil || index == " "
             end
           end
+
           def draw?
             if !won? && full?
               true
             end
           end
+
           def winner
             if won?
               @board[won?[0]]
             end
           end
+
+
           def play
             until over?
               turn
             end
-            if won?
-              puts " Congratudaltions #{winner}!"
-            else if draw?
-              puts " Cat's Game!"
-            end
+            won? ? puts "Congratulations #{winner}!" : puts "Cat's Game!"
+            # if won?
+            #   puts " Congratulations #{winner}!"
+            # elsif draw?
+            #   puts " Cat's Game!"
+            # end
           end
+
 
         end
